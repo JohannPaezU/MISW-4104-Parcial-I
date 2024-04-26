44a23c5e8178aaa7d4790860fd7a0bd021c82f1a
JohannPaezU <js.paezc1@uniandes.edu.co>
2024-04-26 03:16:22 -0500

    plant list component development

--- src/app/plant/plant-list/plant-list.component.spec.ts
+++ src/app/plant/plant-list/plant-list.component.spec.ts
@@ -2,8 +2,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
 import { PlantListComponent } from './plant-list.component';
+
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
--- src/app/plant/plant-list/plant-list.component.ts
+++ src/app/plant/plant-list/plant-list.component.ts
@@ -9,16 +9,28 @@ import { Plant } from '../plant';
 })
 export class PlantListComponent implements OnInit {
   plants: Array<Plant> = [];
+  plantsInformation: Map<string, number> = new Map<string, number>();
 
   constructor(private plantService: PlantService) { }
 
   getPlants(): void {
     this.plantService.getPlants().subscribe(plants => {
       this.plants = plants;
+      this.setplantsInformation(plants);
     });
   }
 
   ngOnInit() {
     this.getPlants();
+  }
+
+  setplantsInformation(plants: Plant[]) {
+    plants.forEach(plant => {
+      let plantType = plant.tipo.toLowerCase();
+      if (!this.plantsInformation.has(plantType)) {
+        this.plantsInformation.set(plantType, 0);
+      }
+      this.plantsInformation.set(plantType, this.plantsInformation.get(plantType)! + 1);
+    });
   }
 }
