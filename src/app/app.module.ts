f1c52a239e914682e8c81160a88ba30e41608434
JohannPaezU <js.paezc1@uniandes.edu.co>
2024-04-26 01:29:33 -0500

    Create plant module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantModule } from './plant/plant.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PlantModule
   ],
   providers: [],
   bootstrap: [AppComponent]
